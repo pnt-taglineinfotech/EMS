c55c179da6bb9af2cb6c9fd9b98adeee9ef74845
Pranav Tondlekar <pnt@taglineinfotech.com>
2026-08-21 14:54:39 +0530

    Updated on 21-08-2026

--- edit.js
+++ edit.js
@@ -1,6 +1,6 @@
 const param = new URLSearchParams( window.location.search );
 
-const goHome = () => { window.location.href = '../'; };
+const goHome = () => { window.location.href = './index.html'; };
 
 const form = document.getElementById( 'form-edit-employee' );
 
